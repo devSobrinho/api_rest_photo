9aea196499d3666ecec0e7d4ed19e9de14094ef8
Dan Sobr <dev.sobrinho@gmail.com>
2021-11-10 11:27:38 -0300

    Created routes user and token

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import user from '../controllers/User';
+import user from '../controllers/UserController';
+// import loginRequired from '../middleware/loginRequired';
 
 const router = new Router();
 
 router.post('/', user.store);
+// router.put('/', loginRequired, user.store;
 
 export default router;
